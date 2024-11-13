9b49c1bb26e5e9c61e2c5b65bde4edf1af89865a
KyungSub Kim <bcong9455@gmail.com>
2024-11-14 03:34:16 +0900

    v

--- src/userscript-header.ts
+++ src/userscript-header.ts
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SOOP (숲) - 채팅 스타일러
 // @namespace    https://github.com/bcong
-// @version      2411140328
+// @version      2411140329
 // @description  새로운 채팅 환경
 // @author       비콩
 // @match        https://www.sooplive.co.kr/*
