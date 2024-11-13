07bb57d7c5d0bcf9f89f78018517d21257a2d797
KyungSub Kim <bcong9455@gmail.com>
2024-11-14 03:32:50 +0900

    v

--- src/userscript-header.ts
+++ src/userscript-header.ts
@@ -1,14 +1,17 @@
 // ==UserScript==
-// @name         SOOP(숲) - 채팅 스타일러
-// @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  사용자가 채팅 환경을 자유롭게 커스터마이징할 수 있는 확장 프로그램입니다.
+// @name         SOOP (숲) - 채팅 스타일러
+// @namespace    https://github.com/bcong
+// @version      2411140328
+// @description  새로운 채팅 환경
 // @author       비콩
 // @match        https://www.sooplive.co.kr/*
 // @match        https://play.sooplive.co.kr/*
 // @match        https://vod.sooplive.co.kr/player/*
-// @grant        none
+// @icon         https://res.sooplive.co.kr/afreeca.ico
 // @github       https://github.com/bcong
 // @connect      sooplive.co.kr
+// @run-at       document-end
+// @license MIT
+// @downloadURL https://github.com/bcong/soop_chat_styler/blob/master/dist/userscripts.user.js
+// @updateURL https://github.com/bcong/soop_chat_styler/blob/master/dist/userscripts.user.js
 // ==/UserScript==
-
