41533410ea975f12086578e1270f951d295f828b
KyungSub Kim <bcong9455@gmail.com>
2024-11-15 18:06:48 +0900

    v

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
-import { PluginOption, defineConfig, splitVendorChunkPlugin } from "vite";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import fs from "fs";
 import path from "path";
 import monkey from 'vite-plugin-monkey';
 
 export default defineConfig(({ mode }) => {
     console.log("Building in", mode);
+
+    const now = new Date();
+    const version = `${now.getFullYear()}${String(now.getMonth() + 1).padStart(2, '0')}${String(now.getDate()).padStart(2, '0')}${String(now.getHours()).padStart(2, '0')}${String(now.getMinutes()).padStart(2, '0')}${String(now.getSeconds()).padStart(2, '0')}`;
+
     return {
         plugins: [
-            // bundlePlugin,
             react({
                 tsDecorators: true,
             }),
@@ -17,7 +19,7 @@ export default defineConfig(({ mode }) => {
                 userscript: {
                     name: 'SOOP (숲) - 채팅 스타일러',
                     namespace: 'https://github.com/bcong',
-                    version: '0',
+                    version: version,
                     description: '새로운 채팅 환경',
                     author: '비콩',
                     match: [
@@ -86,50 +88,3 @@ export default defineConfig(({ mode }) => {
         },
     };
 });
-
-const bundlePlugin: PluginOption = {
-    name: "bundle-plugin",
-    apply: "build",
-    enforce: "post",
-    generateBundle(options, bundle) {
-        let css = "";
-        for (const fileName in bundle) {
-            const chunk = bundle[fileName];
-            if (chunk.type === "asset" && chunk.fileName.endsWith(".css")) {
-                console.log(
-                    "\nFound CSS chunk",
-                    chunk.fileName,
-                    "Inlining and removing from bundle."
-                );
-                css += chunk.source;
-                delete bundle[fileName];
-            }
-        }
-        for (const fileName in bundle) {
-            const chunk = bundle[fileName];
-            if (chunk.type === "chunk") {
-                // This may mess the source map :-(
-                chunk.code = addHeader(chunk.code);
-
-                // Inject the CSS into the bundle
-                chunk.code += `;\n(function(){
-                    const el = document.createElement("style");
-                    el.innerText = ${JSON.stringify(css)};
-                    el.type = "text/css";
-                    document.head.appendChild(el);
-                })();`;
-            }
-        }
-
-        function addHeader(code: string) {
-            const header = fs.readFileSync("src/userscript-header.ts", "utf-8");
-            const now = new Date();
-            const version = `${now.getFullYear()}${String(now.getMonth() + 1).padStart(2, '0')}${String(now.getDate()).padStart(2, '0')}${String(now.getHours()).padStart(2, '0')}${String(now.getMinutes()).padStart(2, '0')}${String(now.getSeconds()).padStart(2, '0')}`;
-            const updatedHeader = header.replace(/@version\s+[\d]+/, `@version      ${version}`);
-
-            console.log("\nAdding header to userscript:\n", updatedHeader);
-
-            return `${updatedHeader}\n${code}`;
-        }
-    },
-};
