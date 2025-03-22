10d07c78a607fcdab9492de22819aa5647d40c38
KyungSub Kim <bcong9455@gmail.com>
2025-03-23 02:53:33 +0900

    채팅 아이템에 타입 추가
    .f

--- src/Components/SettingMenu/index.tsx
+++ src/Components/SettingMenu/index.tsx
@@ -12,6 +12,14 @@ const SettingMenuComponent: React.FC<I_PROPS> = ({
     const id = 'chatStylerSetting';
 
     useEffect(() => {
+        const closeElement = document.getElementById('setbox_close');
+
+        if (closeElement) {
+            closeElement.remove();
+        }
+    }, []);
+
+    useEffect(() => {
         const checkAndInsertElement = () => {
             const serviceUtilElement = document.querySelector('.serviceUtil');
 
