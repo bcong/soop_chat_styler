361761f3cbe315d240ae28bd1e21642e747940de
KyungSub Kim <bcong9455@gmail.com>
2025-07-24 22:14:10 +0900

    Update userscripts.user.js

--- dist/userscripts.user.js
+++ dist/userscripts.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SOOP (숲) - 채팅 스타일러
 // @namespace    https://github.com/bcong
-// @version      20250724221046
+// @version      20250724221405
 // @author       비콩
 // @description  새로운 채팅 환경
 // @license      MIT
