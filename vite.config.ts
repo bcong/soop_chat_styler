38924a845363c7d67590bf6628bdb59cfea627e1
GyeongSeop Kim <bcong9455@gmail.com>
2026-05-29 01:17:22 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig, splitVendorChunkPlugin } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 import monkey from 'vite-plugin-monkey';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig(({ mode }) => {
-    console.log("Building in", mode);
+    console.log('Building in', mode);
 
     const now = new Date();
     const version = `${now.getFullYear()}${String(now.getMonth() + 1).padStart(2, '0')}${String(now.getDate()).padStart(2, '0')}${String(now.getHours()).padStart(2, '0')}${String(now.getMinutes()).padStart(2, '0')}${String(now.getSeconds()).padStart(2, '0')}`;
@@ -17,10 +17,7 @@ export default defineConfig(({ mode }) => {
                 apply: 'serve',
                 configureServer(server) {
                     server.middlewares.use((req, res, next) => {
-                        res.setHeader(
-                            'Access-Control-Allow-Private-Network',
-                            'true'
-                        );
+                        res.setHeader('Access-Control-Allow-Private-Network', 'true');
                         next();
                     });
                 },
@@ -39,41 +36,41 @@ export default defineConfig(({ mode }) => {
                     match: [
                         'https://www.sooplive.co.kr/*',
                         'https://play.sooplive.co.kr/*',
-                        'https://vod.sooplive.co.kr/player/*'
+                        'https://vod.sooplive.co.kr/player/*',
                     ],
                     icon: 'https://res.sooplive.co.kr/afreeca.ico',
                     connect: ['sooplive.co.kr'],
                     grant: ['GM_setValue', 'GM_getValue', 'GM_listValues'],
                     license: 'MIT',
                     downloadURL: 'https://github.com/bcong/soop_chat_styler/blob/master/dist/userscripts.user.js',
-                    updateURL: 'https://github.com/bcong/soop_chat_styler/blob/master/dist/userscripts.user.js'
+                    updateURL: 'https://github.com/bcong/soop_chat_styler/blob/master/dist/userscripts.user.js',
                 },
                 server: {
                     mountGmApi: true,
-                    open: false
+                    open: false,
                 },
                 build: {
-                    fileName: 'userscripts.user.js'
-                }
+                    fileName: 'userscripts.user.js',
+                },
             }),
             svgr(),
             splitVendorChunkPlugin(),
         ],
-        base: "./",
-        root: "./",
+        base: './',
+        root: './',
         build: {
             cssCodeSplit: false,
             cssMinify: false,
             emptyOutDir: false,
-            outDir: "dist",
+            outDir: 'dist',
             minify: false,
             sourcemap: false,
             chunkSizeWarningLimit: 1500,
             lib: {
-                entry: "src/index.tsx",
-                name: "userscript",
+                entry: 'src/index.tsx',
+                name: 'userscript',
                 fileName: () => `userscripts.user.js`,
-                formats: ["iife"],
+                formats: ['iife'],
             },
             rollupOptions: {
                 output: {
@@ -83,22 +80,22 @@ export default defineConfig(({ mode }) => {
             },
         },
         define: {
-            "process.env.NODE_ENV": JSON.stringify(mode),
+            'process.env.NODE_ENV': JSON.stringify(mode),
         },
         resolve: {
             alias: {
-                "@Assets": path.resolve(__dirname, "src/Assets"),
-                "@Types": path.resolve(__dirname, "src/@types"),
-                "@Views": path.resolve(__dirname, "src/Views"),
-                "@Pages": path.resolve(__dirname, "src/Pages"),
-                "@Templates": path.resolve(__dirname, "src/Templates"),
-                "@Models": path.resolve(__dirname, "src/Models"),
-                "@Utils": path.resolve(__dirname, "src/Utils"),
-                "@Components": path.resolve(__dirname, "src/Components"),
-                "@Stores": path.resolve(__dirname, "src/Stores"),
-                "@Styles": path.resolve(__dirname, "src/Styles"),
-                "@Widgets": path.resolve(__dirname, "src/Widgets"),
-                "@Module": path.resolve(__dirname, "src/Module"),
+                '@Assets': path.resolve(__dirname, 'src/Assets'),
+                '@Types': path.resolve(__dirname, 'src/@types'),
+                '@Views': path.resolve(__dirname, 'src/Views'),
+                '@Pages': path.resolve(__dirname, 'src/Pages'),
+                '@Templates': path.resolve(__dirname, 'src/Templates'),
+                '@Models': path.resolve(__dirname, 'src/Models'),
+                '@Utils': path.resolve(__dirname, 'src/Utils'),
+                '@Components': path.resolve(__dirname, 'src/Components'),
+                '@Stores': path.resolve(__dirname, 'src/Stores'),
+                '@Styles': path.resolve(__dirname, 'src/Styles'),
+                '@Widgets': path.resolve(__dirname, 'src/Widgets'),
+                '@Module': path.resolve(__dirname, 'src/Module'),
             },
         },
     };
